92b9415e50214ebd6407629740e8af740d6d2050
gourab8389 <deyg6988@gmail.com>
2024-11-08 02:38:16 +0530

    feat: add updateVectorDB utility for document processing and integrate with API

--- pages/api/updatedatabase.ts
+++ pages/api/updatedatabase.ts
@@ -1,8 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import { Pinecone } from "@pinecone-database/pinecone";
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { TextLoader } from "langchain/document_loaders/fs/text";
-import { NextApiRequest, NextApiResponse } from "next";
+
+import { updateVectorDB } from "@/utils";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if(req.method === "POST"){
@@ -22,4 +24,16 @@ async function handleUpload(indexName:string, namespace: string, res: NextApiRes
     const client = new Pinecone({
         apiKey: process.env.PINECONE_API_KEY!
     })
+    await updateVectorDB(client, indexName, namespace, docs, (filename, totalChunks, chunksUpserted, isComplete) => {
+        if(isComplete){
+            JSON.stringify({
+                filename,
+                totalChunks,
+                chunksUpserted,
+                isComplete
+            })
+        }else{
+            res.end();
+        }
+    });
 }
