b05854d234760cdaefe1d8dcf09220388f37afa1
gourab8389 <deyg6988@gmail.com>
2024-11-07 23:39:21 +0530

    first commit

---
+++ app/pinecone/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const VectorDbPage = (props: Props) => {
+  return (
+    <div className='flex flex-col items-center p-24'>
+
+    </div>
+  )
+}
+
+export default VectorDbPage
