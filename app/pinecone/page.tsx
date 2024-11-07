60c45c13a09a191b3e49c4ef5dfdef16187c3d7f
gourab8389 <deyg6988@gmail.com>
2024-11-07 23:47:54 +0530

    add pinecone db page

--- app/pinecone/page.tsx
+++ app/pinecone/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Database } from 'lucide-react'
 
 type Props = {}
 
 const VectorDbPage = (props: Props) => {
   return (
     <div className='flex flex-col items-center p-24'>
+        <Card>
+            <CardHeader>
+                <CardTitle>Update Knowledge Base</CardTitle>
+                <CardDescription>
+                    Add new documents to vector DB
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="grid grid-cols-3 gap-4">
+                    <div className="col-span-2 grid gap-4 border rounded-lg p-6">
 
+                    </div>
+                    <Button variant={"outline"} className='w-full h-full'>
+                        <Database size={50}/>
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
     </div>
   )
 }
